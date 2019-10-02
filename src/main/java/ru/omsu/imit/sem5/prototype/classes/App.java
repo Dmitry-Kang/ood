3919af9367d58ab1094c28e290348619b7c7420d
Dmitriy Alexandrov <alexandrov.dp@mail.ru>
2019-10-03 00:21:41 +0600

    # in prototype pattern: + added it + added test

---
+++ src/main/java/ru/omsu/imit/sem5/prototype/classes/App.java
@@ -0,0 +1,54 @@
+package ru.omsu.imit.sem5.prototype.classes;
+
+import java.util.Objects;
+
+public abstract class App {
+    private String name;
+    private int size;
+
+    public App() {}
+
+    public App(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public App(App app) {
+        if (app != null) {
+            this.name = app.getName();
+            this.size = app.getSize();
+        }
+    }
+
+    public abstract App clone();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        App app = (App) o;
+        return size == app.size &&
+                Objects.equals(name, app.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, size);
+    }
+}
