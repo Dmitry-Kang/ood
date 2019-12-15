84613d8ed66c30dfceeac0b4c6208d9dcf156ac0
Dmitriy_Alexandrov <omihka3@gmail.com>
2019-12-15 13:03:23 +0600

    + done flyweight

--- src/main/java/ru/omsu/imit/sem5/structural/flyweight/RoomWithWalls.java
+++ src/main/java/ru/omsu/imit/sem5/structural/flyweight/RoomWithWalls.java
@@ -7,10 +7,12 @@ public class RoomWithWalls {
     private BulletTypeFactory btf;
 
     public RoomWithWalls(int wallX) {
-        bulletType.state = State.FieldTested;
-        bulletType.type = Type.ArmorPiercing;
+        bulletType = new BulletType(Type.ArmorPiercing, State.FieldTested);
+
         this.wallX = wallX;
+
         weapon = new Weapon(bulletType);
+
         btf = new BulletTypeFactory();
         btf.add(bulletType);
     }
@@ -18,12 +20,20 @@ public class RoomWithWalls {
     public void work() {
         BulletType brokenBulletType = new BulletType(Type.ArmorPiercing, State.Broken);
         btf.add(brokenBulletType);
+        boolean stop = false;
 
-        weapon.addAmmo();
-        weapon.fire();
-        for (Bullet bullet: weapon.getAmmo()) {
-            if (bullet.getX() > 100) {
-                bullet.setType(btf.get(State.Broken));
+        while (true) {
+            weapon.addAmmo();
+            weapon.fire();
+            for (Bullet bullet: weapon.getAmmo()) {
+                if (bullet.getX() > wallX) {
+                    bullet.setType(btf.get(State.Broken));
+                    stop = true;
+                    break;
+                }
+            }
+            if (stop) {
+                break;
             }
         }
     }
